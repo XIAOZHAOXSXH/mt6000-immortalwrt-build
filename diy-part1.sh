1fad0f572e188f330ad23a0278e7d55722144d82
XIAOZHAOXSXH <112842045+XIAOZHAOXSXH@users.noreply.github.com>
2026-02-19 22:36:17 +0000

    init

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 描述: 更新 feeds 前的自定义脚本
+
+# 示例：添加一个第三方插件包源
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
---
+++ diy-part2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 描述: 更新 feeds 后的自定义脚本
+
+# 示例：修改默认后台 IP 为 192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 示例：修改主机名
+sed -i 's/ImmortalWrt/GL-MT6000/g' package/base-files/files/bin/config_generate
